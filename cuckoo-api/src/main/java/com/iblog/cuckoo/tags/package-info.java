4f83edf58a5cba04ff4d934b437f7ed0298acd2a
xushaoxiao <xushaoxiao@flashhold.com>
2019-06-04 00:12:04 +0800

    resolve conflict of .gitignore file

---
+++ cuckoo-api/src/main/java/com/iblog/cuckoo/tags/package-info.java
@@ -0,0 +1,9 @@
+//
+
+/**
+ *  Api for associating tags scoped operations.
+ *
+ *  <p>This package manages a set of tags in the {@code io.grpc.Context}. The tags can be used to
+ *  label anything that is associated with a specific. For example, the {@code }</p>
+ */
+package com.iblog.cuckoo.tags;
