908a4fcf2f735074ab1e7a14aef0447475d79ed4
xushaoxiao <xushaoxiao@flashhold.com>
2019-06-05 11:07:50 +0800

    fill TagValue

--- cuckoo-api/src/main/java/com/cuckoo/tags/TagValue.java
+++ cuckoo-api/src/main/java/com/cuckoo/tags/TagValue.java
@@ -16,5 +16,59 @@
 
 package com.cuckoo.tags;
 
-public class TagValue {
+import javax.annotation.concurrent.Immutable;
+
+import com.cuckoo.internal.StringUtils;
+import com.cuckoo.internal.Utils;
+import com.google.auto.value.AutoValue;
+
+@Immutable
+@AutoValue
+public abstract class TagValue {
+
+    /**
+     * The maximum length for a tag value. The value is {@value #MAX_LENGTH}.
+     *
+     * @since 0.0.1
+     */
+    public static final int MAX_LENGTH = 255;
+
+    TagValue() {}
+
+    /**
+     * Constructs a {@code TagValue} from given string. the string must meet the following
+     * requirements:
+     *
+     * <ol>
+     *     <li>It cannot be longer than {@link #MAX_LENGTH}</li>
+     *     <li>It can only contain printable ASCII characters.</li>
+     * </ol>
+     *
+     * @param value the tag value.
+     * @return a {@code TagValue} from the given string.
+     * @throws IllegalArgumentException if the {@code String} is not valid.
+     * @since 0.0.1
+     */
+    public static TagValue create(String value) {
+        Utils.checkArgument(isValid(value), "Invalid TagValue: %s", value);
+        return new AutoValue_TagValue(value);
+    }
+
+    /**
+     * Returns the tag value as a {@code String}.
+     *
+     * @return the tag value as a {@code String}.
+     * @since 0.0.1
+     */
+    public abstract String asString();
+
+    /**
+     * Determines whether the given {@code String} is a valid tag value.
+     *
+     * @param value the tag value to be validated.
+     * @return whether the value is valid.
+     */
+    private static boolean isValid(String value) {
+        return value.length() <= MAX_LENGTH && StringUtils.isPrintableString(value);
+    }
 }
